75a72a9178d16a91972220f6b8537c890fce71ba
Nahuel Rodriguez <nahuerodcast@gmail.com>
2022-09-28 22:59:17 +0000

    adding animations

--- components/Content/HomeContent.js
+++ components/Content/HomeContent.js
@@ -22,170 +22,177 @@ export const HomeContent = () => {
 
   return (
     <Container
-      lg
       css={{
         padding: 0,
-        background: `radial-gradient(circle, rgba(80,9,121,${
-          isDark ? "0.3" : "0.1"
-        }) 0%, rgba(0,0,0,0) 70%)`,
+        // background: `radial-gradient(circle, rgba(80,9,121,${
+        //   isDark ? "0.3" : "0.1"
+        // }) 0%, rgba(0,0,0,0) 70%)`,
       }}
-      className={`mainAnimation animate__animated animate__fadeIn`}
+      // className={`mainAnimation animate__animated animate__fadeIn`}
     >
-      <div
-        style={{
+      <Container
+        lg
+        css={{
           display: "flex",
-          alignItems: "center",
           flexDirection: "row",
-          maxWidth: "$lg",
-          minHeight: "80vh",
+          justifyContent: "space-between",
         }}
       >
-        <Container>
-          <Text
-            h1
-            css={{
-              textGradient: "45deg, $blue600 -20%, $pink600 50%",
-              paddingBottom: 0,
-              marginBottom: 0,
-              fontSize: "$7xl",
-            }}
-          >
-            {title}
-          </Text>
-          <Text
-            h3
-            css={{
-              paddingTop: 0,
-              marginTop: "-10px",
-            }}
-          >
-            BA, Argentina.
-          </Text>
-          <Text
-            weight="500"
-            size="$xl"
-            css={{ w: "65%", lineHeight: "24px" }}
-            color="$gray700"
-          >
-            {desc}
-          </Text>
-          <Button
-            shadow
-            css={{
-              backgroundColor: "$accents9",
-              normalShadow: "$accents4",
-              marginTop: "24px",
-              color: "$accents0",
-            }}
-            onClick={() => {
-              setShowMoreTransition(true);
-              ref.current.scrollIntoView({
-                top: 0,
-                left: 0,
-                behavior: "smooth",
-              });
-            }}
-          >
-            {showMore}
-          </Button>
-          <Spacer y={1} />
-        </Container>
-        <Card
-          isHoverable
-          borderWeight="extrabold"
-          css={{
-            width: "320px",
-            height: "320px",
-            borderRadius: "52px",
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            flexDirection: "row",
+            minHeight: "80vh",
           }}
         >
-          <Card.Image
-            src="https://portfolio-nahue-rodcast.vercel.app/profile-pic.jpg"
-            objectFit="cover"
-            width="100%"
-            height={500}
-            alt="Nahue Rodcast"
-          />
-        </Card>
-      </div>
-      <div
-        style={{
-          display: "flex",
-          alignItems: "center",
-          flexDirection: "row",
-          maxWidth: "$lg",
-          minHeight: "80vh",
-        }}
-        ref={ref}
-      >
-        <Container>
-          <Text
-            h1
+          <Container>
+            <Text
+              h1
+              css={{
+                textGradient: "45deg, $blue600 -20%, $pink600 50%",
+                paddingBottom: 0,
+                marginBottom: 0,
+                fontSize: "$7xl",
+              }}
+            >
+              {title}
+            </Text>
+            <Text
+              h3
+              css={{
+                paddingTop: 0,
+                marginTop: "-10px",
+              }}
+            >
+              BA, Argentina.
+            </Text>
+            <Text
+              weight="500"
+              size="$xl"
+              css={{ w: "65%", lineHeight: "24px" }}
+              color="$gray700"
+            >
+              {desc}
+            </Text>
+            <Button
+              shadow
+              css={{
+                backgroundColor: "$accents9",
+                normalShadow: "$accents4",
+                marginTop: "24px",
+                color: "$accents0",
+              }}
+              onClick={() => {
+                setShowMoreTransition(true);
+                ref.current.scrollIntoView({
+                  top: 0,
+                  left: 0,
+                  behavior: "smooth",
+                });
+              }}
+            >
+              {showMore}
+            </Button>
+            <Spacer y={1} />
+          </Container>
+          <Card
+            isHoverable
+            borderWeight="extrabold"
             css={{
-              textGradient: "45deg, $blue600 -20%, $pink600 50%",
-              paddingBottom: 0,
-              marginBottom: 0,
-              fontSize: "$7xl",
+              width: "320px",
+              height: "320px",
+              borderRadius: "52px",
             }}
           >
-            {title}
-          </Text>
-          <Text
-            h3
-            css={{
-              paddingTop: 0,
-              marginTop: "-10px",
-            }}
-          >
-            BA, Argentina.
-          </Text>
-          <Text
-            weight="500"
-            size="$xl"
-            css={{ w: "65%", lineHeight: "24px" }}
-            color="$gray700"
-          >
-            {desc}
-          </Text>
-          <Button
-            shadow
-            css={{
-              backgroundColor: "$accents9",
-              normalShadow: "$accents4",
-              marginTop: "24px",
-              color: "$accents0",
-            }}
-            onClick={() => {
-              setShowMoreTransition(true);
-              window.scrollTo({
-                top: 0,
-                left: 0,
-                behavior: "smooth",
-              });
-            }}
-          >
-            {showMore}
-          </Button>
-          <Spacer y={1} />
-        </Container>
-        <Card
-          isHoverable
-          borderWeight="extrabold"
-          css={{
-            width: "320px",
-            height: "320px",
-            borderRadius: "52px",
+            <Card.Image
+              src="https://portfolio-nahue-rodcast.vercel.app/profile-pic.jpg"
+              objectFit="cover"
+              width="100%"
+              height={500}
+              alt="Nahue Rodcast"
+            />
+          </Card>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            flexDirection: "row",
+            maxWidth: "$lg",
+            minHeight: "80vh",
           }}
+          ref={ref}
         >
-          <Card.Image
-            src="https://portfolio-nahue-rodcast.vercel.app/profile-pic.jpg"
-            objectFit="cover"
-            width="100%"
-            height={500}
-            alt="Nahue Rodcast"
-          />
-        </Card>
-      </div>
+          <Container>
+            <Text
+              h1
+              css={{
+                textGradient: "45deg, $blue600 -20%, $pink600 50%",
+                paddingBottom: 0,
+                marginBottom: 0,
+                fontSize: "$7xl",
+              }}
+            >
+              {title}
+            </Text>
+            <Text
+              h3
+              css={{
+                paddingTop: 0,
+                marginTop: "-10px",
+              }}
+            >
+              BA, Argentina.
+            </Text>
+            <Text
+              weight="500"
+              size="$xl"
+              css={{ w: "65%", lineHeight: "24px" }}
+              color="$gray700"
+            >
+              {desc}
+            </Text>
+            <Button
+              shadow
+              css={{
+                backgroundColor: "$accents9",
+                normalShadow: "$accents4",
+                marginTop: "24px",
+                color: "$accents0",
+              }}
+              onClick={() => {
+                setShowMoreTransition(true);
+                window.scrollTo({
+                  top: 0,
+                  left: 0,
+                  behavior: "smooth",
+                });
+              }}
+            >
+              {showMore}
+            </Button>
+            <Spacer y={1} />
+          </Container>
+          <Card
+            isHoverable
+            borderWeight="extrabold"
+            css={{
+              width: "320px",
+              height: "320px",
+              borderRadius: "52px",
+            }}
+          >
+            <Card.Image
+              src="https://portfolio-nahue-rodcast.vercel.app/profile-pic.jpg"
+              objectFit="cover"
+              width="100%"
+              height={500}
+              alt="Nahue Rodcast"
+            />
+          </Card>
+        </div>
+      </Container>
     </Container>
   );
 };
--- components/Layout.js
+++ components/Layout.js
@@ -6,6 +6,7 @@ import {
   Avatar,
   useTheme,
   Switch,
+  Container,
 } from "@nextui-org/react";
 import useDarkMode from "use-dark-mode";
 import { useIntl } from "react-intl";
@@ -44,68 +45,83 @@ export const Layout = ({ children }) => {
         justifyContent: "space-between",
       }}
     >
-      <Navbar variant="sticky" maxWidth={"lg"}>
-        <Navbar.Brand>
-          <Avatar
-            css={{ marginRight: "12px" }}
-            src="https://portfolio-nahue-rodcast.vercel.app/profile-pic.jpg"
-            size="sm"
-          />
-          <NextLink href="/" locale={locale}>
-            <Text css={{ marginRight: "4px", cursor: "pointer" }}>
-              {" "}
-              Nahue Rodcast{" "}
-            </Text>
-          </NextLink>
-        </Navbar.Brand>
-        <Navbar.Content hideIn="xs" variant="highlight-rounded">
-          <NextLink href={`/`} locale={locale}>
-            <Navbar.Link isActive={asPath === "/"}>{aboutMe}</Navbar.Link>
-          </NextLink>
-          <NextLink href={`/projects`} locale={locale}>
-            <Navbar.Link isActive={asPath.includes("projects")}>
-              {projects}
-            </Navbar.Link>
-          </NextLink>
-          <NextLink href={`/experience`} locale={locale}>
-            <Navbar.Link isActive={asPath.includes("experience")}>
-              {experience}
-            </Navbar.Link>
-          </NextLink>
-          <NextLink href={`/contact`} locale={locale}>
-            <Navbar.Link isActive={asPath.includes("contact")}>
-              {contact}
-            </Navbar.Link>
-          </NextLink>
-        </Navbar.Content>
-        <Navbar.Content>
-          <Switch
-            checked={darkMode.value}
-            shadow
-            iconOn={<SunIcon filled />}
-            iconOff={<MoonIcon filled />}
-            onChange={() => darkMode.toggle()}
-          />
-          {[...locales].sort().map(
-            (loc, id) =>
-              !locale.includes(loc) && (
-                <NextLink key={id} href={asPath} locale={loc}>
-                  <div
-                    style={{
-                      cursor: "pointer",
-                    }}
-                  >
-                    <Flag code={loc === "es" ? "arg" : "usa"} height="16" />
-                  </div>
-                </NextLink>
-              )
-          )}
-        </Navbar.Content>
+      <Navbar variant="sticky" maxWidth={"xl"}>
+        <div
+          style={{
+            width: "100%",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+        >
+          <Container
+            lg
+            css={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+            }}
+          >
+            <Navbar.Brand>
+              <Avatar
+                css={{ marginRight: "12px" }}
+                src="https://portfolio-nahue-rodcast.vercel.app/profile-pic.jpg"
+                size="sm"
+              />
+              <NextLink href="/" locale={locale}>
+                <Text css={{ marginRight: "4px", cursor: "pointer" }}>
+                  {" "}
+                  Nahue Rodcast{" "}
+                </Text>
+              </NextLink>
+            </Navbar.Brand>
+            <Navbar.Content hideIn="xs" variant="highlight-rounded">
+              <NextLink href={`/`} locale={locale}>
+                <Navbar.Link isActive={asPath === "/"}>{aboutMe}</Navbar.Link>
+              </NextLink>
+              <NextLink href={`/projects`} locale={locale}>
+                <Navbar.Link isActive={asPath.includes("projects")}>
+                  {projects}
+                </Navbar.Link>
+              </NextLink>
+              <NextLink href={`/experience`} locale={locale}>
+                <Navbar.Link isActive={asPath.includes("experience")}>
+                  {experience}
+                </Navbar.Link>
+              </NextLink>
+              <NextLink href={`/contact`} locale={locale}>
+                <Navbar.Link isActive={asPath.includes("contact")}>
+                  {contact}
+                </Navbar.Link>
+              </NextLink>
+            </Navbar.Content>
+            <Navbar.Content>
+              <Switch
+                checked={darkMode.value}
+                shadow
+                iconOn={<SunIcon filled />}
+                iconOff={<MoonIcon filled />}
+                onChange={() => darkMode.toggle()}
+              />
+              {[...locales].sort().map(
+                (loc, id) =>
+                  !locale.includes(loc) && (
+                    <NextLink key={id} href={asPath} locale={loc}>
+                      <div
+                        style={{
+                          cursor: "pointer",
+                        }}
+                      >
+                        <Flag code={loc === "es" ? "arg" : "usa"} height="16" />
+                      </div>
+                    </NextLink>
+                  )
+              )}
+            </Navbar.Content>
+          </Container>
+        </div>
       </Navbar>
-      <div
-        className="mainAnimation"
-        // style={{ width: "100%", padding: 0, margin: 0 }}
-      >
+      <div className={isDark ? "mainAnimation" : "lightAnimation"}>
         {children}
       </div>
       <Navbar maxWidth={"lg"}>
